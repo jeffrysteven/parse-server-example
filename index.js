5a8bc7ee6aa8353cf7158ede8b691b00ffe9c9ac
Jeffry Steven Lenis Jimenez <jeffry_steven@MacBook-Pro-de-Jeffry-2.local>
2017-02-23 21:13:48 -0500

    push notifications fix.

--- index.js
+++ index.js
@@ -21,7 +21,12 @@ var api = new ParseServer({
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
   verifyUserEmails: true,
-  push: {}
+  push: {
+    android: {
+      senderId: 'YOUR_GOOGLE_DEVELOPER_CONSOLE_PROJECT_NUMBER_HERE', // The Sender ID of GCM
+      apiKey: 'YOUR_GOOGLE_DEVELOPER_CONSOLE_API_KEY_HERE' // The Server API Key of GCM
+    }
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
